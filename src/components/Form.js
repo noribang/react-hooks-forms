a3008c73ebdb138e6b957dce7ad5851e0d7b6349
hoonkim <hoonk9387@gmail.com>
2022-06-07 21:38:27 -0500

    Added form checkbox. Added state. Added event listener and event handler.

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,26 +1,50 @@
 import React, { useState } from "react";
 
+// function Form() {
+//   const [firstName, setFirstName] = useState("John");
+//   const [lastName, setLastName] = useState("Henry");
+
+//   /* Event handler callback */
+//   function handleFirstNameChange(event) {
+//     setFirstName(event.target.value);
+//   }
+
+//   function handleLastNameChange(event) {
+//     setLastName(event.target.value);
+//   }
+
+//   return (
+//     <form>
+//       {/* Event listener={event handler callback} */}
+//       <input type="text" onChange={handleFirstNameChange} value={firstName} />
+//       <input type="text" onChange={handleLastNameChange} value={lastName} />
+//       <button type="submit">Submit</button>
+//     </form>
+//   );
+// }
+
 function Form() {
-  const [firstName, setFirstName] = useState("John");
-  const [lastName, setLastName] = useState("Henry");
+  const [newsletter, setNewsletter] = useState(false);
 
   /* Event handler callback */
-  function handleFirstNameChange(event) {
-    setFirstName(event.target.value);
-  }
-
-  function handleLastNameChange(event) {
-    setLastName(event.target.value);
+  function handleNewsletterChange(event) {
+    setNewsletter(event.target.checked);
+    console.log(newsletter);
   }
 
   return (
     <form>
-      {/* Event listener={event handler callback} */}
-      <input type="text" onChange={handleFirstNameChange} value={firstName} />
-      <input type="text" onChange={handleLastNameChange} value={lastName} />
+      <label htmlFor="newsletter">Subscribe to our Newsletter? </label>
+      <input 
+        type="checkbox"
+        id="newsletter"
+        /* Event listener={callback} */
+        onChange={handleNewsletterChange}
+        checked={newsletter}
+      />
       <button type="submit">Submit</button>
     </form>
-  );
+  )  
 }
 
 export default Form;
