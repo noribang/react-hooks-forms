996abcf64fcfe8756e2b81a18b0f15ec74a5b567
hoonkim <hoonk9387@gmail.com>
2022-06-07 20:31:23 -0500

    Npm install. Added event listener and event handler to form input.

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,10 +4,20 @@ function Form() {
   const [firstName, setFirstName] = useState("John");
   const [lastName, setLastName] = useState("Henry");
 
+  /* Event handler callback */
+  function handleFirstNameChange(event) {
+    setFirstName(event.target.value);
+  }
+
+  function handleLastNameChange(event) {
+    setLastName(event.target.value);
+  }
+
   return (
     <form>
-      <input type="text" value={firstName} />
-      <input type="text" value={lastName} />
+      {/* Event listener={event handler callback} */}
+      <input type="text" onChange={handleFirstNameChange} value={firstName} />
+      <input type="text" onChange={handleLastNameChange} value={lastName} />
       <button type="submit">Submit</button>
     </form>
   );
